adccab3bace47f4642612dfbcc5dff1f1dbcfb13
Leonardo Moreira <lmoreira597@gmail.com>
2022-07-21 17:01:17 -0300

    conclusão do css na porta e integração typeScript

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,15 +1,20 @@
+import PortaModel from "../model/porta";
 import styles from "../styles/Porta.module.css"
 
-export default function Porta(props) {
+interface PortaProps{
+    porta: PortaModel
+}
 
-    const selecionada = props.selecionada ? styles.selecionada : ""
+export default function Porta(props: PortaProps) {
+    const { porta } = props
+    const selecionada = porta.selecionada ? styles.selecionada : ""
 
     return (
         <div className={styles.area}>
             <div className={`${styles.estrutura} ${selecionada}`}>
                 <div className={styles.porta}>
                     <div className={styles.numero}>
-                        3
+                        {porta.numero}
                     </div>
                     <div className={styles.macaneta}>
                         
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Porta from "../components/Porta";
+import PortaModel from "../model/porta";
+import React from "react";
 
 export default function Home() {
+  const [p1, setP1] = React.useState(new PortaModel(1, false, true))
   return (
     <div style={{display: "flex"}}>
-      <Porta selecionada/>
-      <Porta/>
+      <Porta porta={p1}/>
     </div>
   )
 }
