af606487e615a3b20b29ac3d394ba67c22fc9df4
Leonardo Moreira <lmoreira597@gmail.com>
2022-07-21 17:29:11 -0300

    alteração do estado da porta

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [p1, setP1] = React.useState(new PortaModel(1, false, false))
   return (
     <div style={{display: "flex"}}>
-      <Porta value={p1}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
     </div>
   )
 }
