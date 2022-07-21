81a54e9415a9b3dd020518bd0fa2463726892ba7
Leonardo Moreira <lmoreira597@gmail.com>
2022-07-21 17:14:35 -0300

    adicionando o value na porta

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import PortaModel from "../model/porta";
 import React from "react";
 
 export default function Home() {
-  const [p1, setP1] = React.useState(new PortaModel(1, false, true))
+  const [p1, setP1] = React.useState(new PortaModel(1, false, false))
   return (
     <div style={{display: "flex"}}>
-      <Porta porta={p1}/>
+      <Porta value={p1}/>
     </div>
   )
 }
