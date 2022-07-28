2c110f6aa62235196d378751b50f7896bd390eb4
Leonardo Moreira <lmoreira597@gmail.com>
2022-07-28 17:12:26 -0300

    implementando link no index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import Porta from "../components/Porta";
-import PortaModel from "../model/porta";
 import React from "react";
-import { atualizarPortas, criarPortas } from "../functions/portas";
+import Cartao from "../components/Cartao";
+import style from "../styles/Formulario.module.css";
+import Link from "../node_modules/next/link";
 
-export default function Home() {
+export default function Formulario() {
 
   return (
-    <h1>
-      Formulario de inicio
-    </h1>
+    <div className={style.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao></Cartao>children
+        <Cartao></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`}>
+            <a>
+              <h2>
+                iniciar
+              </h2>
+            </a>
+          </Link>
+        </Cartao>
+      </div>
+    </div>
   )
 }
